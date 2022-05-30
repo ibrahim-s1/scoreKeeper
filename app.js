9e066d59c9663637d8e4039a37e608cb453a5623
Ibrahim S <ibsofficials@gmail.com>
2022-05-30 19:15:51 +0100

    added slight styling elements

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ p1Btn.addEventListener("click", function() {
             p1Score += 1;
             if (p1Score === winningScore) {
                 isGameOver = true;
+                p1Display.classList.add("winner")
+                p2Display.classList.add("loser")
             }
             p1Display.textContent = p1Score;
         }
@@ -27,6 +29,8 @@ p2Btn.addEventListener("click", function() {
             p2Score += 1;
             if (p2Score === winningScore) {
                 isGameOver = true;
+                p2Display.classList.add("winner")
+                p2Display.classList.add("loser")
             }
             p2Display.textContent = p2Score;
         }
@@ -46,4 +50,6 @@ function reset() {
     p1Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove("winner", "loser")
+    p2Display.classList.remove("winner", "loser")
 }
